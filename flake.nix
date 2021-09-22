f87d9454ee3399c8e83f715d423dca2231573c29
Jarl André Hübenthal <jarlah@protonmail.com>
2021-09-22 09:10:08 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     home-manager.url = "github:nix-community/home-manager";
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = inputs: {
@@ -17,11 +18,42 @@
           ./configuration.nix
           inputs.nixos-hardware.nixosModules.dell-xps-13-9310
           inputs.home-manager.nixosModules.home-manager
+          inputs.impermanence.nixosModules.impermanence
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jarlandre = import ./home-manager.nix;
-            home-manager.users.work = import ./home-manager.nix;
+            home-manager.users.jarlandre = { pkgs, ... }: {
+              imports = [
+                "${inputs.impermanence}/home-manager.nix"
+                ./home/starship.nix
+                ./home/zsh.nix
+              ];
+              programs.home-manager.enable = true;
+              programs.firefox.enable = true;
+              programs.fzf.enable = true;
+              programs.htop.enable = true;
+
+              home.persistence."/nix/persist/home/jarlandre" = {
+                directories = [ ".ssh" "Downloads" ".mozilla" ];
+                files = [];
+              };
+            }
+            home-manager.users.work = { pkgs, ... }: {
+              imports = [
+                "${inputs.impermanence}/home-manager.nix"
+                ./home/starship.nix
+                ./home/zsh.nix
+              ];
+              programs.home-manager.enable = true;
+              programs.firefox.enable = true;
+              programs.fzf.enable = true;
+              programs.htop.enable = true;
+
+              home.persistence."/nix/persist/home/work" = {
+                directories = [ ".ssh" "Downloads" ".mozilla" ];
+                files = [];
+              };
+            }
           }
           {
             config.nixpkgs = {
--- home-manager.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [
-    ./home/starship.nix
-    ./home/zsh.nix
-  ];
-  programs.firefox.enable = true;
-  programs.fzf.enable = true;
-  programs.htop.enable = true;
-}
